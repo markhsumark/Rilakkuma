2346b7fd024c978e8ab376d27cfbb05571a31198
markhsumark <tulsa0415@gmail.com>
2021-10-17 13:58:40 +0800

    Initial Commit

---
+++ Rilakkuma/RilakkumaApp.swift
@@ -0,0 +1,17 @@
+//
+//  RilakkumaApp.swift
+//  Rilakkuma
+//
+//  Created by 徐易中 on 2021/10/17.
+//
+
+import SwiftUI
+
+@main
+struct RilakkumaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
